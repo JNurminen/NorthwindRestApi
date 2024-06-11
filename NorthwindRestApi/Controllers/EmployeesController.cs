3395c420ca496db57bb5b307d6601c5d5258ad88
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2024-06-11 11:29:03 +0300

    Add project files.

---
+++ NorthwindRestApi/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // Luodaan tietokantayhteys
+        NorthwindContext db = new NorthwindContext();
+
+        // Hae kaikki työntekijät
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var tyontekijat = db.Employees.ToList();
+            return Ok(tyontekijat);
+        }
+    }
+}
---
+++ NorthwindRestApi/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Luodaan tietokantayhteys
+        NorthwindContext db = new NorthwindContext();
+
+        // Hae kaikki tuotteet
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var tuotteet = db.Products.ToList();
+            return Ok(tuotteet);
+        }
+    }
+}
